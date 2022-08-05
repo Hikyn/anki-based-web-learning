3dd92ae7fe403571372326ba376813bb3814ed7c
Danila <danila10danila@maill.ru>
2022-08-05 23:14:06 +0300

    Added support for sql database for button EnglishA1

--- app.py
+++ app.py
@@ -15,10 +15,20 @@ db = SQL("sqlite:///app.db")
 
 @app.route("/")
 def index():
+    EnglishA1 = db.execute("SELECT * FROM EnglishA1 ORDER BY category ASC")
     print("User visited index page")
-    return render_template("index.html")
+    return render_template("index.html", EnglishA1=EnglishA1)
+
+# @app.route("/add")
+# def add():
+    if request.method == "POST":
+        table = request.form.get("table")
+        category = request.form.get("category")
+    print("User visited add page")
+    return render_template("add.html")
 
 
 # Tests for urls at the start of flask server
 with app.test_request_context():
     print(url_for("index"))
+    # print(url_for("add"))

0c6230c9f185a4f8ed14576c6189df851c4f93ed
Danila <danila10danila@mail.ru>
2022-08-18 03:51:15 +0300

    Trying to fix errors with database

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import date, timedelta
 import random
 import os
+import re
 from werkzeug.security import check_password_hash, generate_password_hash
 from functools import wraps
 
@@ -20,11 +21,14 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=28)
 
 # Configure CS50 Library to use SQLite database
 #db = SQL("sqlite:///app.db")
-SQLALCHEMY_DATABASE_URI = os.environ.get('postgres://hphffpxfwzsooz:d05122abcf50369c16faab99e383b32c55e0146ec47db763c51f501506e86faa@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/df6gjgtv751q0f')
+#SQLALCHEMY_DATABASE_URI = os.environ.get('postgres://hphffpxfwzsooz:d05122abcf50369c16faab99e383b32c55e0146ec47db763c51f501506e86faa@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/df6gjgtv751q0f')
 #uri = os.getenv("postgres://hphffpxfwzsooz:d05122abcf50369c16faab99e383b32c55e0146ec47db763c51f501506e86faa@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/df6gjgtv751q0f")
 #if uri.startswith("postgres://"):
 #    uri = uri.replace("postgres://", "postgresql://")
-db = SQLAlchemy(app)
+uri = os.getenv("postgres://hphffpxfwzsooz:d05122abcf50369c16faab99e383b32c55e0146ec47db763c51f501506e86faa@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/df6gjgtv751q0f")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+db = SQL(uri)
 
 # Check if there are any tables with that name and user_id
 def tableExists(table):
